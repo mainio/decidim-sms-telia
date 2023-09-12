4fd439aad8c7c6f71c8ebc356ee262533fa62912
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-09-12 13:58:38 +0300

    Remove token_generator file

--- lib/decidim/sms/telia.rb
+++ lib/decidim/sms/telia.rb
@@ -6,7 +6,6 @@ module Decidim
   module Sms
     # This namespace holds the logic for Telia SMS integration.
     module Telia
-      autoload :TokenGenerator, "decidim/sms/telia/token_generator"
       autoload :Gateway, "decidim/sms/telia/gateway"
     end
   end

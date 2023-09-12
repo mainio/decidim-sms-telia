375e4dfcbe1fcd40347c0489e3800b9851bb6f35
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-09-12 10:40:18 +0300

    Refactor deliveries table based on telia specifications

--- db/migrate/20230912073543_create_decidim_sms_telia_deliveries.rb
+++ db/migrate/20230912073543_create_decidim_sms_telia_deliveries.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
-class CreateDecidimSmsTeliaDeliveries < ActiveRecord::Migration[6.0]
+class CreateDecidimSmsTeliaDeliveries < ActiveRecord::Migration[6.1]
   def change
     create_table :decidim_sms_telia_deliveries do |t|
       t.string :from
       t.string :to
-      t.string :body
-      t.string :sid
       t.string :status
+      t.string :resource_url
+      t.string :callback_data
 
       t.timestamps
     end

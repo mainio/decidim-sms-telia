2849aa15f62f818383414b159ad3078433a9bad8
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-09-12 11:39:30 +0300

    Fix a bug in delivery

--- app/models/decidim/sms/telia/delivery.rb
+++ app/models/decidim/sms/telia/delivery.rb
@@ -9,18 +9,18 @@ module Decidim
 
         def generate_callback_code
           loop do
-            self.callback_code = digest
+            self.callback_code = generate_digest
             if ::Decidim::Sms::Telia::Delivery.find_by(callback_code: callback_code).blank?
               save!
             end
           end
+        end
 
-          private
+        private
 
-          def digest
-            characters = ("0".."9").to_a + ("A".."Z").to_a + ("a".."a").to_a
-            characters.sample(32).join
-          end
+        def generate_digest
+          characters = ("0".."9").to_a + ("A".."Z").to_a + ("a".."a").to_a
+          characters.sample(32).join
         end
       end
     end

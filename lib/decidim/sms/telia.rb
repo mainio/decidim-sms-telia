191efb9d9f34ba6f930467d3a305e5b52640a006
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-09-18 14:24:01 +0300

    Make the following changes:
    - Add the configuration option for the delay for resending SMS
    in case the server was busy.
    - Add the token_generator for email generations.

--- lib/decidim/sms/telia.rb
+++ lib/decidim/sms/telia.rb
@@ -7,6 +7,13 @@ module Decidim
     # This namespace holds the logic for Telia SMS integration.
     module Telia
       autoload :Gateway, "decidim/sms/telia/gateway"
+      autoload :TokenManager, "decidim/sms/telia/token_manager"
+      include ActiveSupport::Configurable
+      # Default configuration wait interval before retry sending a queued sms that was
+      # failed due to a busy server.
+      config_accessor :sms_retry_delay do
+        10
+      end
     end
   end
 end
